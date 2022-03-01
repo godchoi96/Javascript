1b0a5b9f5bf61bcc01278033d5193922db63a1e2
godChoi96 <oinkoink71@naver.com>
2022-03-02 08:21:22 +0900

    edit - 중첩 describe문 전까지 완료

--- js/test.js
+++ js/test.js
@@ -1,8 +1,11 @@
 describe("pow", function () {
-    it("2의 3제곱은 8입니다.", function () {
-        assert.equal(pow(2, 3), 8);
-    });
-    it("3의 4제곱은 81입니다.", function () {
-        assert.equal(pow(3, 4), 81);
-    });
+    function makeTest(x) {
+        let expected = x * x * x;
+        it(`${x}을 3번 곱하면 ${expected}입니다.`, function () {
+            assert.equal(pow(x, 3), expected);
+        });
+    }
+    for (let x = 1; x <= 5; x++) {
+        makeTest(x);
+    }
 });
