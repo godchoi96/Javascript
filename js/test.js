b04c55450da6ea6fa47c36971004a69e68fb052b
godChoi96 <oinkoink71@naver.com>
2022-03-02 08:11:17 +0900

    edit - 220302.md

--- js/test.js
+++ js/test.js
@@ -1,5 +1,8 @@
 describe("pow", function () {
-    it("주어진 숫자의 n 제곱", function () {
+    it("2의 3제곱은 8입니다.", function () {
         assert.equal(pow(2, 3), 8);
+    });
+    it("3의 4제곱은 81입니다.", function () {
+        assert.equal(pow(3, 4), 81);
     });
 });
